c0671f027b161fa86eaa19b8d79ebee629106f5a
Edivin joão <edivinjoao3@gmail.com>
2025-09-30 21:22:36 +0100

    add aula nova

---
+++ aula2-variaveis/script.js
@@ -0,0 +1,36 @@
+/* Variaveis
+
+    const (CONSTANTE)
+    let ()
+    var (NÃO RECOMENDADO USAR)
+
+*/
+
+const abacate = "Cor verde"
+const numero = 30
+
+console.log(abacate)
+console.log(numero)
+
+/* Tipos de dados
+
+    Strings (Textos) ""
+    Numbers (Numeros])
+    Boolean (Booleano)
+    Object (Objectos)
+    Null & Undefined
+    Array (Matriz)
+
+*/
+
+const nome = "João"     /*Strings*/
+const idade = 25        /*Numbers*/
+const casado = false    /*Boolean*/
+    
+    /*Obejactos*/
+const user = {
+    nome: `João`,
+    idade: 25,
+    casado: false
+}
+console.log(user)
