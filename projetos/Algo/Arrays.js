26021b6d115d07d5b253a0d61077a8ddee3d3b55
Edivin joão <edivinjoao3@gmail.com>
2025-10-16 22:40:24 +0100

    fiz alguma coisa

---
+++ projetos/Algo/Arrays.js
@@ -0,0 +1,9 @@
+console.log("Olá pessoas!");
+
+//ARRAYS
+
+const animes = ['Dr.Stone', 'Naruto', 'Solo Leveling'];
+
+animes.push('Dragon Ball Z'); //adiciona um item no final do array
+animes.unshift('One Punsh Man'); //adiciona um item no começo do array
+console.log(animes);
---
+++ projetos/Criando CRUDs/script.js
@@ -0,0 +1,20 @@
+console.log("Olá, pessoas!");
+
+// CRUD - JavaScrips
+const miniLista = {
+    usuarios: [
+        nome = 'João',
+    ],
+    post: [
+        user = 'João',
+        mensagem = 'Olá, pessoas!'
+    ],
+};
+
+// CREATE
+function criarPost() {
+    
+}
+// READ
+// UPDADE
+// DELETE
